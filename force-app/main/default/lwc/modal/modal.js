5a87f7ae9bef9eb4620906bdfebb7a3ba634cfef
shkang <shkang@trestle.co.kr>
2023-11-02 11:09:25 +0900

    Rewrite jsdoc, Apply esc to close

--- force-app/main/default/lwc/modal/modal.js
+++ force-app/main/default/lwc/modal/modal.js
@@ -1,128 +1,130 @@
-import { LightningElement, api } from "lwc";
-
 /**
- * @alias 		Modal
+ * @module 		Modal
  * @extends 	LightningElement
- * @classdesc 	Custom modal component
+ * @description	Custom modal component
  * @author 		shkang <shkang@i2max.co.kr>
- * @hideconstructor
  * @example
+ *
  * <c-modal width="50%">
  *     <span slot="header"></span>
  *     <span slot="body"></span>
  *     <span slot="footer"></span>
  * <c-modal>
  */
+
+/* --------------------------------------------------------------------------------------------------------
+* Import
+-------------------------------------------------------------------------------------------------------- */
+import { LightningElement, api } from "lwc";
+
 export default class Modal extends LightningElement {
-    /**************************************************
-     * Public
-     **************************************************/
-
-    /**
-     * @private
-     * @type {string | undefined}
-     */
-    _width;
-
+    /* --------------------------------------------------------------------------------------------------------
+	* Public Property
+	-------------------------------------------------------------------------------------------------------- */
     /**
      * Modal width
-     * @memberof Modal
+     *
      * @public
      * @type {string}
+     * @param {number|string} value
      */
     @api
-    get width() {
-        return this._width;
-    }
     set width(value) {
         this._width = typeof value === "number" ? value + "px" : value;
     }
+    get width() {
+        return this._width;
+    }
+    _width;
 
     /**
-     * @private
+     * Hide close button
+     *
+     * @public
      * @type {boolean}
-     */
-    _hideClose = false;
-
-    /**
-     * @memberof Modal
-     * @type {boolean}
+     * @param {boolean|string} value
      */
     @api
-    get hideClose() {
-        return this._hideClose;
-    }
     set hideClose(value) {
         this._hideClose = this.toBoolean(value);
     }
+    get hideClose() {
+        return this._hideClose;
+    }
+    _hideClose = false;
 
     /**
-     * @private
-     * @type {boolean}
-     */
-    _hideHeader = false;
-
-    /**
-     * @memberof Modal
+     * Hide modal header
+     *
      * @public
      * @type {boolean}
+     * @param {boolean|string} value
      */
     @api
-    get hideHeader() {
-        return this._hideHeader;
-    }
     set hideHeader(value) {
         this._hideHeader = this.toBoolean(value);
     }
+    get hideHeader() {
+        return this._hideHeader;
+    }
+    _hideHeader = false;
 
     /**
-     * @private
-     * @type {boolean}
-     */
-    _hideFooter = false;
-
-    /**
-     * @memberof Modal
+     * Hide modal footer
+     *
      * @public
      * @type {boolean}
+     * @param {boolean|string} value
      */
     @api
-    get hideFooter() {
-        return this._hideFooter;
-    }
     set hideFooter(value) {
         this._hideFooter = this.toBoolean(value);
     }
+    get hideFooter() {
+        return this._hideFooter;
+    }
+    _hideFooter = false;
 
     /**
-     * Open the modal
-     * @memberof Modal
+     * Open modal
+     *
      * @public
-     * @example
-     * this.template.querySelector("c-modal").open();
+     * @method
      */
     @api open() {
         this._isModalOpen = true;
+
+        window.addEventListener("keydown", this._keyDownHandler);
     }
 
     /**
-     * Close the modal
-     * @memberof Modal
+     * Fire close event then close modal
+     *
      * @public
-     * @author shkang <shkang@i2max.co.kr>
-     * @example
-     * this.template.querySelector("c-modal").close();
+     * @method
+     * @fires module:Modal~close
      */
     @api close() {
+        /**
+         * @event module:Modal~close
+         */
+        this.dispatchEvent(new CustomEvent("close"));
+
+        window.removeEventListener("keydown", this._keyDownHandler);
+
         this._isModalOpen = false;
     }
 
-    /**************************************************
-     * Private
-     **************************************************/
-
+    /* --------------------------------------------------------------------------------------------------------
+	* Private Property
+	-------------------------------------------------------------------------------------------------------- */
     _isModalOpen = false;
+    _isRendered = false;
+    _keyDownHandler = this.handleKeyDown.bind(this);
 
+    /* --------------------------------------------------------------------------------------------------------
+	* Getter / Setter
+	-------------------------------------------------------------------------------------------------------- */
     get modalClass() {
         return `slds-modal ${this._isModalOpen ? "slds-fade-in-open" : ""}`;
     }
@@ -135,11 +137,34 @@ export default class Modal extends LightningElement {
         return `width:${this._width ? this._width : "50%"} !important;`;
     }
 
+    /* --------------------------------------------------------------------------------------------------------
+	* Event Handler
+	-------------------------------------------------------------------------------------------------------- */
+    handleCloseClick() {
+        this.close();
+    }
+
+    handleKeyDown(event) {
+        if (event.keyCode === 27) {
+            // ESC to close
+            this.close();
+        }
+    }
+
+    handleBodyKeyDown(event) {
+        // prevent ESC to close while focusing the body elements
+        event.stopPropagation();
+    }
+
+    /* --------------------------------------------------------------------------------------------------------
+	* Utility
+	-------------------------------------------------------------------------------------------------------- */
     /**
      * Convert boolean like string to actual boolean
-     * @memberof Modal
+     *
      * @private
-     * @param {string | boolean} value boolean like string
+     * @method
+     * @param {boolean|string} value `boolean` or boolean like `string`
      * @returns {boolean}
      */
     toBoolean(value) {
